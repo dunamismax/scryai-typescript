ba3f450f3f44aec5210cb6c1ff6eb56d802af2ea
dunamismax <dunamismax@tutamail.com>
2026-02-17 17:04:20 -0500

    rm

--- web-template/app/controllers/dashboards_controller.rb
+++
@@ -1,8 +0,0 @@
-class DashboardsController < ApplicationController
-  before_action :authenticate_user!
-
-  def show
-    @snapshot = DashboardSnapshot.fetch(current_user)
-    @projects = policy_scope(Project).recent_first.limit(5)
-  end
-end
